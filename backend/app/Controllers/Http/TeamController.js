c0e7036d61dbac7c3fff5050e63bcced5bc5780c
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-02-08 11:57:54 +0000

    middlewares

--- backend/app/Controllers/Http/TeamController.js
+++ backend/app/Controllers/Http/TeamController.js
@@ -11,13 +11,11 @@ class TeamController {
   /**
    * Show a list of all teams.
    * GET teams
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index ({auth}) {
+    const teams  = await auth.user.teams().fetch()
+
+    return teams
   }
 
   /**
@@ -29,64 +27,53 @@ class TeamController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async create ({ request, response, view }) {
-  }
 
-  /**
-   * Create/save a new team.
-   * POST teams
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
+  async store ({ request, auth }) {
+    const data = request.only(['name'])
+
+    const team = await auth.user.teams().create({
+      ...data,
+      user_id:auth.user.id,
+    })
+    return team
   }
 
   /**
    * Display a single team.
    * GET teams/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
+     */
+  async show ({params,auth }) {
+    const team = await auth.user.teams().where('teams.id', params.id).first()
+
+    return team
   }
 
   /**
    * Render a form to update an existing team.
    * GET teams/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async edit ({ params, request, response, view }) {
-  }
 
-  /**
-   * Update team details.
-   * PUT or PATCH teams/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
+  async update ({ params, request, auth }) {
+    const data = request.only(['name'])
+    const team = await auth.user.teams().where('teams.id', params.id).first()
+
+    team.merge(data)
+    await team.save()
+
+    return team
+
   }
 
   /**
    * Delete a team with id.
    * DELETE teams/:id
    *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
+
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params, auth }) {
+    const team = await auth.user.teams().where('teams.id', params.id).first()
+
+    await team.delete()
   }
 }
 
--- backend/start/routes.js
+++ backend/start/routes.js
@@ -17,3 +17,11 @@
 const Route = use('Route')
 
 Route.post('sessions', 'SessionController.store')
+
+Route.group(()=>{
+  Route.resource('teams' , 'TeamController').apiOnly()
+}).middleware('auth');
+
+Route.group(()=>{
+  Route.post('invites' , 'InviteController.store')
+}).middleware(['auth', 'team']);
